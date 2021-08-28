6b9fdc821dfd0eb3988a0f3fc4f4852a15366c64
Lucas Cascão <lucas.cascao@fatec.sp.gov.br>
2021-08-28 19:17:12 -0300

    Alterado formato da data e configuração de banco

--- client-module-server/src/main/resources/db/migration/V04__inserindo_clientes_e_usuarios.sql
+++ client-module-server/src/main/resources/db/migration/V04__inserindo_clientes_e_usuarios.sql
@@ -3,14 +3,14 @@ values ('896564841','lucas_cascao_@outlook.com','CFF1A29A5F758BD2D52448F236EFB64
 ('656226568','leonardo@gmail.com','CFF1A29A5F758BD2D52448F236EFB640', 1, 2),
 ('989898599','nataniprado26@gmail.com','CFF1A29A5F758BD2D52448F236EFB640', 1,2),
 ('598774598','joaocascao@yahoo.com.br','CFF1A29A5F758BD2D52448F236EFB640', 1, 2),
-('899985985','jocascao@yahoo.com.br','CFF1A29A5F758BD2D52448F236EFB640', 1, 2);
+('899985985','jocascao@yahoo.com.br','CFF1A29A5F758BD2D52448F236EFB640', 1, 2)
 
 insert into cliente (cli_nome , cli_sobrenome, cli_sexo, cli_data_nascimento, cli_tcl_id, cli_usu_id)
-values ('Lucas','De Almeida Rama Cascão','M','16/09/1995' ,1,1),
-('Leonardo','De Almeida Rama Cascão', 'M', '10/01/1997',1,2),
-('Natani','Prado Cascão', 'F', '26/09/1999',1,3),
-('João','Rama Cascão', 'M', '07/04/1965',1,4),
-('Josueli','Keler De Almeida','F','17/09/1967',1,5);
+values ('Lucas','De Almeida Rama Cascão','M','1995-09-16' ,1,1),
+('Leonardo','De Almeida Rama Cascão', 'M', '1997-01-10',1,2),
+('Natani','Prado Cascão', 'F', '1999-09-26',1,3),
+('João','Rama Cascão', 'M', '1965-04-07',1,4),
+('Josueli','Keler De Almeida','F','1967-09-17',1,5);
 
 insert into documento (doc_codigo, doc_data_validade, doc_tdo_id, doc_cli_id)
 values ('23265746801', '2026-01-01', 1,1),
